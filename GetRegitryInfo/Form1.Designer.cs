7492ec5b97a40482ba36151a67727e83784d558c
Rodriguez <u09033@healthnow.org>
2020-09-29 18:14:16 -0400

    added Files

---
+++ GetRegitryInfo/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GetRegitryInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxDisplayResults = new System.Windows.Forms.TextBox();
+            this.textBoxStringInput = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan System";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxDisplayResults
+            // 
+            this.textBoxDisplayResults.Location = new System.Drawing.Point(12, 60);
+            this.textBoxDisplayResults.Multiline = true;
+            this.textBoxDisplayResults.Name = "textBoxDisplayResults";
+            this.textBoxDisplayResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxDisplayResults.Size = new System.Drawing.Size(776, 338);
+            this.textBoxDisplayResults.TabIndex = 2;
+            // 
+            // textBoxStringInput
+            // 
+            this.textBoxStringInput.Location = new System.Drawing.Point(12, 34);
+            this.textBoxStringInput.Name = "textBoxStringInput";
+            this.textBoxStringInput.Size = new System.Drawing.Size(245, 20);
+            this.textBoxStringInput.TabIndex = 3;
+            this.textBoxStringInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxStringInput_KeyDown);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(120, 434);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(542, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Software to Find";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBoxStringInput);
+            this.Controls.Add(this.textBoxDisplayResults);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Get Local Machine Registry Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxDisplayResults;
+        private System.Windows.Forms.TextBox textBoxStringInput;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GetRegitryInfo/Form1.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GetRegitryInfo
+{
+    public partial class Form1 : Form
+    {
+        private string SoftwareName;
+        private string Reg;
+        List<String> Apps = new List<String>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ClearForm();
+
+            textBoxDisplayResults.Text = "";
+            
+            progressBar1.Value = 0;
+            Reg = "";
+            SoftwareName = textBoxStringInput.Text;
+
+            Apps = GetUnintstallString(SoftwareName);
+
+            if (Apps.Count != 0)
+            {
+
+                foreach (string found in Apps)
+                {
+                    Reg += found + Environment.NewLine;
+                }
+
+                textBoxDisplayResults.Text = Reg;
+            }
+            else
+            {
+                label2.ForeColor = Color.FromName("RED");
+                label2.Text = "Software not found";
+
+            }
+
+        }
+
+        private List<string> GetUnintstallString(string Software)
+        {
+            if (Software == "")
+            {
+                var invalidEntry = new List<string>();
+
+                return invalidEntry;
+            }
+
+            String DisplayName, UninstallString, Path, DispalyVersion, finalOutput;
+            var softwareFound = new List<string>();
+            var uninstallReg = new List<RegistryKey>();
+
+
+            uninstallReg.Add(Registry.LocalMachine.OpenSubKey(@"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"));
+            uninstallReg.Add(Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall"));
+
+            foreach (RegistryKey Archi in uninstallReg)
+            {
+                string[] regKeys = Archi.GetSubKeyNames();
+
+                for (int i = 0; i < regKeys.Length; i++)
+                {
+
+                    progressBar1.PerformStep();
+
+                    if (regKeys[i] != null)
+                    {
+                        RegistryKey keySearch = Archi.OpenSubKey(regKeys[i]);
+                        if (keySearch != null)
+                        {
+                            try
+                            {
+
+                                if (keySearch.GetValue("DisplayName").ToString().ToLower().Contains(Software.ToLower()))
+                                {
+
+                                    DisplayName ="DisplayName: " + keySearch.GetValue("DisplayName").ToString();
+                                    UninstallString ="UninstallString: " + keySearch.GetValue("UninstallString").ToString();
+                                    Path = "Path: " +keySearch.Name;
+                                    DispalyVersion = "DispalyVersion: " + keySearch.GetValue("DisplayVersion").ToString();
+
+                                    finalOutput = DisplayName + Environment.NewLine + 
+                                        UninstallString + Environment.NewLine + 
+                                        DispalyVersion + Environment.NewLine +
+                                        Path + Environment.NewLine + 
+                                        "===================================================================";
+                                    softwareFound.Add(finalOutput);
+                                }
+                            }
+                            catch (NullReferenceException e)
+                            {
+                                Console.WriteLine(e);
+                            }
+                        }
+                    }
+
+
+                }
+            }
+            return softwareFound;
+        }
+
+
+        private void ClearForm()
+        {
+            label2.Text = "";
+            label2.ForeColor = Color.FromName("Black");
+            textBoxDisplayResults.Text = "";
+            progressBar1.Value = 0;
+            progressBar1.Refresh();
+
+        }
+
+        private void textBoxStringInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+
+                button1.PerformClick();
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+    }
+}
